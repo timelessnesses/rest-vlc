5f494b1d94f5374c573bcf8d08d25346e5aa6ffb
timelessnesses <mooping3roblox@gmail.com>
2022-04-02 14:19:34 +0700

    i suppose to make this in new branch

--- rest_vlc.py
+++ rest_vlc.py
@@ -501,42 +501,37 @@ else:
         async def get(*args, **kwargs):
             async with aiohttp.ClientSession() as session:
                 async with session.get(*args, **kwargs) as response:
-                    d = dummy()
-                    d.status_code = response.status
-                    d.text = await response.text()
-            return d
+                    pass
+            response.status_code = response.status
+            return response
 
         async def post(*args, **kwargs):
             async with aiohttp.ClientSession() as session:
                 async with session.post(*args, **kwargs) as response:
-                    d = dummy()
-                    d.status_code = response.status
-                    d.text = await response.text()
-            return d
+                    pass
+            response.status_code = response.status
+            return response
 
         async def put(*args, **kwargs):
             async with aiohttp.ClientSession() as session:
                 async with session.put(*args, **kwargs) as response:
-                    d = dummy()
-                    d.status_code = response.status
-                    d.text = await response.text()
-            return d
+                    pass
+            response.status_code = response.status
+            return response
 
         async def patch(*args, **kwargs):
             async with aiohttp.ClientSession() as session:
                 async with session.patch(*args, **kwargs) as response:
-                    d = dummy()
-                    d.status_code = response.status
-                    d.text = await response.text()
-            return d
+                    pass
+            response.status_code = response.status
+            return response
 
         async def delete(*args, **kwargs):
             async with aiohttp.ClientSession() as session:
                 async with session.delete(*args, **kwargs) as response:
-                    d = dummy()
-                    d.status_code = response.status
-                    d.text = await response.text()
-            return d
+                    pass
+            response.status_code = response.status
+            return response
 
     class Async_VLC:
         def __init__(
